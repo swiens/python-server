6e16a2079f7415f77e951ad0c5308378b6f1ec7a
Susan Wiens <susanmichellewiens@gmail.com>
2021-01-14 11:30:04 -0600

    chapter 4 post updates

--- customers/request.py
+++ customers/request.py
@@ -37,3 +37,19 @@ def get_single_customer(id):
             requested_customer = customer
 
     return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last animal in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    customer["id"] = new_id
+
+    # Add the animal dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
-from locations import get_all_locations, get_single_location
-from employees import get_all_employees, get_single_employee
-from customers import get_all_customers, get_single_customer
+from animals import get_all_animals, get_single_animal, create_animal
+from locations import get_all_locations, get_single_location, create_location
+from employees import get_all_employees, get_single_employee, create_employee
+from customers import get_all_customers, get_single_customer, create_customer
+import json
+
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -88,11 +90,36 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_POST(self):
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_post = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_post = create_animal(post_body)
+
+        if resource == "locations":
+            new_post = create_location(post_body)
+
+        if resource == "employees":
+            new_post = create_employee(post_body)
+            
+        if resource == "customers":
+            new_post = create_customer(post_body)
+            
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_post}".encode())
+
 
 
     # Here's a method on the class that overrides the parent's method.
